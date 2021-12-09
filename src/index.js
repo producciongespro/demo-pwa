ce715197e526bd5d31ef9286bfb002815686aad2
lchaconc <luis.chacon.campos@gmail.com>
2021-12-09 13:55:47 -0600

    Imp de Sw -registro de Ws manjeado de eventos para instal, activate y fetch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 console.log("Inicia carga React DOM");
+
+if (navigator.serviceWorker) {
+  console.log("Registrando Service Worker");
+  navigator.serviceWorker.register("sw.js");
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 console.log("React DOM cargado");
